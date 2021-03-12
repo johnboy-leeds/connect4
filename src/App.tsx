2551156f8b5688057dd5fff51de85db0d56d0c8e
John Hickling <john.hickling@coveainsurance.co.uk>
2021-03-12 22:36:44 +0000

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,22 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
+import { MainMenu, Game } from './components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
-import { PlayArea } from './components';
-import { Counter, Column, Grid, GameState } from './modules/GameEngine';
-
-const newGame = (): GameState => ({
-    grid: new Grid(),
-    status: 'active',
-    turn: Counter.red,
-    winner: undefined,
-    redStack: 21,
-    yellowStack: 21,
-});
 
 function App(): ReactElement {
-    const [gameState, setGameState] = useState<GameState>(newGame());
-    const { grid } = gameState;
-
-    const handleAddCounter = (col: Column): void => {
-        if (gameState.status !== 'active') {
-            return;
-        }
-
-        const newGameState = {
-            ...gameState,
-        };
-        if (gameState.turn === Counter.red) {
-            newGameState.turn = Counter.yellow;
-            newGameState.redStack--;
-        } else {
-            newGameState.turn = Counter.red;
-            newGameState.yellowStack--;
-        }
-
-        col.addCounter(gameState.turn);
-        const gridWinner = grid.getWinner();
-        console.log('did it', gridWinner);
-        if (gridWinner) {
-            newGameState.status = 'complete';
-            newGameState.winner = gridWinner;
-        }
-        if (grid.isFull()) {
-            newGameState.status = 'stalemate';
-        }
-
-        setGameState(newGameState);
-    };
-
-    const handleReset = (): void => {
-        setGameState(newGame());
-    };
-
     return (
-        <div className="App">
-            <PlayArea
-                onAddCounter={handleAddCounter}
-                onReset={handleReset}
-                gameState={gameState}
-            />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route path="/game">
+                        <Game />
+                    </Route>
+                    <Route path="/">
+                        <MainMenu />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
